1d93608391b32041d5e94244870435a986318696
Sakshi Bainsla <87930446+bainslasakshi@users.noreply.github.com>
2022-05-15 21:41:53 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,22 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+      apiKey: "AIzaSyAs1iWJCdDrwaZWUi39RVa2yvp7Hlasy3c",
+      authDomain: "class-test-97f0b.firebaseapp.com",
+      databaseURL: "https://class-test-97f0b-default-rtdb.firebaseio.com",
+      projectId: "class-test-97f0b",
+      storageBucket: "class-test-97f0b.appspot.com",
+      messagingSenderId: "224374168947",
+      appId: "1:224374168947:web:de79e339d107033fba7906"
+    };
+  
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
